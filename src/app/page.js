22e3e1276a3628ff1f91a0a2588e7e8720173a89
Satyam Kumar <113851151+SKumr20@users.noreply.github.com>
2025-04-18 14:06:56 +0530

    Change: Hero BackgroundGradient colors

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Hero from "@/components/sections/Hero";
 
 export default function Home() {
   return (
    <>
+    <Hero />
    </>
   );
 }
