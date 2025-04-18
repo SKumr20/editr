f0c0bf55d033b39b748c04698239ed5dc330c2e6
Satyam Kumar <113851151+SKumr20@users.noreply.github.com>
2025-04-19 00:58:23 +0530

    Fix: make nav logo as link

--- src/components/sections/Navbar.jsx
+++ src/components/sections/Navbar.jsx
@@ -9,10 +9,10 @@ const Navbar = () => {
     <div className="fixed top-4 left-0 right-0 z-50 flex items-center justify-between p-2 md:p-3 mx-10 md:mx-20 lg:mx:50 xl:mx-80 bg-background backdrop-blur-md rounded-lg border border-border shadow-xs">
       <div className="flex items-center gap-2">
         {/* Left - Title and logo */}
-        <div className="flex items-center gap-2">
+        <Link href='/' className="flex items-center gap-2 hover:opacity-60 transition duration-200">
           <PencilLine strokeWidth={1.5} />
           <h2 className="font-bold" >Editr</h2>
-        </div>
+        </Link>
         {/* Nav links */}
         <div>
           <Link href='/editor'>
