032600bc77933e99b85c1ce9d33b601abce3719a
Satyam Kumar <113851151+SKumr20@users.noreply.github.com>
2025-04-18 16:22:52 +0530

    Fix: Hero styles

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -9,15 +9,15 @@ const Hero = () => {
         <div className="font-extrabold text-6xl md:text-7xl flex flex-col items-center">
           <p className="leading-tight">The all in one editor for your</p>
           <div className="flex flex-wrap items-center justify-center">
-            <div className="inline-block overflow-hidden">
+            <div className="inline-block overflow-hidden mt-2">
               <TextFlip className="text-7xl" />
             </div>
             <span className="ml-2 md:ml-4">needs.</span>
           </div>
         </div>
         {/* Desctiption text */}
-        <p className="text-2xl font-light tracking-tight max-w-xs sm:max-w-md md:max-w-2xl lg:max-w-4xl mx-auto">
-          Editr is a simple, free and easy to use online editor, with all the necessary features, clean UI, and more. Make quick notes, share and collaborate easily, and much more!
+        <p className="text-3xl opacity-60 font-light tracking-tighter max-w-xs sm:max-w-md md:max-w-2xl lg:max-w-4xl mx-auto">
+          Editr is a simple, free and easy to use online editor, with all the necessary features, clean UI to make quick notes, share and collaborate easily.
         </p>
       </div>
     </BackgroundGradientAnimation>
