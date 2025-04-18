979392a2d9e7d2c1f646008e47948f31dd55b95b
Satyam Kumar <113851151+SKumr20@users.noreply.github.com>
2025-04-18 13:29:09 +0530

    feat: add new button type - Nav

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,13 @@
-import { Geist, Geist_Mono, The_Nautigal } from "next/font/google";
+import { Inter } from "next/font/google";
 import "../styles/globals.css";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import Navbar from "@/components/sections/Navbar";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const inter = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Editr",
@@ -22,7 +18,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.variable} antialiased`}
       >
         <ThemeProvider>
           <Navbar />
--- src/components/sections/Navbar.jsx
+++ src/components/sections/Navbar.jsx
@@ -1,15 +1,28 @@
 import ToggleDarkmode from "../ui/ToggleDarkmode";
 import { PencilLine } from "lucide-react";
 import GithubSmallBtn from "../ui/GithubSmallbtn";
+import Link from "next/link";
+import { Button } from "../ui/button";
 
 const Navbar = () => {
   return (
     <div className="fixed top-0 left-0 right-0 z-50 flex items-center justify-between p-2 md:p-3 mt-6 mx-10 md:mx-20 lg:mx:50 xl:mx-80 bg-background rounded-lg border border-border shadow-xs">
-      {/* Left - Title and logo */}
+      <div className="flex items-center gap-2">
+        {/* Left - Title and logo */}
         <div className="flex items-center gap-2">
           <PencilLine strokeWidth={1.5} />
           <h2 className="font-bold" >Editr</h2>
         </div>
+        {/* Nav links */}
+        <div>
+          <Link href='/editor'>
+            <Button variant='nav' size='sm'>Get started</Button>
+          </Link>
+          <Link href='#'>
+            <Button variant='nav' size='sm'>Changelog</Button>
+          </Link>
+        </div>
+      </div>
         {/* Right side div */}
         <div className="flex items-center gap-2">
           <GithubSmallBtn />
