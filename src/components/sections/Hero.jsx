7a19dea57ed524a23aecee50a80c71307138a5e9
Satyam Kumar <113851151+SKumr20@users.noreply.github.com>
2025-04-18 15:15:40 +0530

    Fix: Responsiveness

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -4,19 +4,21 @@ import { TextFlip } from "../ui/TextFlip";
 const Hero = () => {
   return (
     <BackgroundGradientAnimation>
-      <div className="absolute z-50 inset-0 mx-10 flex flex-col gap-10 items-center justify-center-safe px-4 pointer-events-none text-center">
+      <div className="absolute z-50 inset-0 flex flex-col gap-10 items-center justify-center px-4 pointer-events-none text-center">
         {/* Heading div with textflip */}
         <div className="font-extrabold text-7xl flex flex-col gap-4 items-center">
-          <p>The all in one editor for your</p>
-          <p className="flex items-center justify-center">
-            <span className="inline-block">
-              <TextFlip className="text-3xl md:text-4xl lg:text-7xl" />
-            </span>
-            <span className="ml-4">needs</span>
-          </p>
+          <p className="leading-tight">The all in one editor for your</p>
+          <div className="flex flex-wrap items-center justify-center">
+            <div className="inline-block overflow-hidden">
+              <TextFlip className="text-7xl" />
+            </div>
+            <span className="ml-2 md:ml-4">needs</span>
+          </div>
         </div>
         {/* Desctiption text */}
-        <p className="text-2xl font-light tracking-tight mx-10 md:mx-40">Editr is a simple, free and easy to use online editor, with all the necessary features, clean UI, and more. Make quick notes, share and collaborate easily, and much more! </p>
+        <p className="text-2xl font-light tracking-tight max-w-xs sm:max-w-md md:max-w-2xl lg:max-w-4xl mx-auto">
+          Editr is a simple, free and easy to use online editor, with all the necessary features, clean UI, and more. Make quick notes, share and collaborate easily, and much more!
+        </p>
       </div>
     </BackgroundGradientAnimation>
   )
