c8f9fd52f6105b5be28c3b493fffb0ab6495c07d
Satyam Kumar <113851151+SKumr20@users.noreply.github.com>
2025-04-19 01:21:38 +0530

    Add Blocknote Editor

--- src/app/editor/page.jsx
+++ src/app/editor/page.jsx
@@ -1,4 +1,7 @@
-import Editor from "@/components/editor/Editor"
+/******* IMP ********/
+// USE THE DYNAMIC VERSION ONLY
+import { Editor } from "../../components/editor/DynamicEditor";
+
 const page = () => {
   return (
     <div className="mt-30 mx-10 md:mx-40">
