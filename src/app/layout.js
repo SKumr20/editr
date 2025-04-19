af208dbd68e339d6cbbe2aa32f2c544f977e1a7d
Satyam Kumar <113851151+SKumr20@users.noreply.github.com>
2025-04-20 01:21:47 +0530

    Add Clerk auth Add convex

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,12 +2,12 @@ import { Inter } from "next/font/google";
 import "../styles/globals.css";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import Navbar from "@/components/sections/Navbar";
+import { ConvexClientProvider } from "../../providers/convex-client-provider"; // Your custom provider
 
 const inter = Inter({
   variable: "--font-inter",
   subsets: ["latin"],
 });
-
 
 export const metadata = {
   title: "Editr",
@@ -17,15 +17,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${inter.variable} antialiased`}
-      >
-        <ThemeProvider>
-          <Navbar />
-          <main>
-            {children}
-          </main>
-        </ThemeProvider>
+      <body className={`${inter.variable} antialiased`}>
+        <ConvexClientProvider>
+          <ThemeProvider>
+            <Navbar />
+            <main>{children}</main>
+          </ThemeProvider>
+        </ConvexClientProvider>
       </body>
     </html>
   );
