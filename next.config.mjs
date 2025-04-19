36cfb5c1d4cee64d728d6a6dc9e54f34d3884806
Satyam Kumar <113851151+SKumr20@users.noreply.github.com>
2025-04-20 01:39:24 +0530

    Fix import alias

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
-/** @type {import('next').NextConfig} */
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const nextConfig = {
-    reactStrictMode: false
+  webpack(config) {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
+  }
 };
 
 export default nextConfig;
